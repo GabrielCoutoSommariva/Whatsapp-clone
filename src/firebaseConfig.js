e02c521b22502fa38966b94bb50a32cf640fe8b4
GabrielCoutoSommariva <coutogabriel122@gmail.com>
2023-12-12 17:07:56 -0300

    whatsclone-app

---
+++ src/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: "AIzaSyCuCmfzZFlFbpsD54wRdC3ZIJ05qR0YaQ4",
+  authDomain: "whatsappclone-d1710.firebaseapp.com",
+  projectId: "whatsappclone-d1710",
+  storageBucket: "whatsappclone-d1710.appspot.com",
+  messagingSenderId: "751563267176",
+  appId: "1:751563267176:web:40bbb4931bed8957a7efdd",
+  measurementId: "G-ETKEFETSTT"
+};
